b89076839fb74fb32bf2215f0efd417c089279eb
Kostiantyn Kaimov <kokaimov@c1b3c1.42berlin.de>
2024-04-13 23:58:28 +0200

    first

---
+++ philo/includes/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kokaimov <kokaimov@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/13 13:48:29 by kokaimov          #+#    #+#             */
+/*   Updated: 2024/04/13 13:49:22 by kokaimov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdbool.h>
+# include <limits.h>
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+#endif
